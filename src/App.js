8cc0e9634e419c2a8f56496ac9380ecc576659ca
Michael J. Lyons <mikelyons7@me.com>
2018-07-16 15:58:14 -0600

    Add stateless component called grocery list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import GroceryListItem from './GroceryListItem';
+
+const items = [
+  {name: 'Bananas'},
+  {name: 'Pears'},
+  {name: 'Kiwis'},
+]
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Evil Deeds</h1>
+        <h1>Grocery List</h1>
+        {
+          items.map((item, i) => <GroceryListItem key={'Glist-' + i} name={item.name} /> )
+        }
       </div>
     );
   }
